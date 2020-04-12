9cc4cc359e9b2b0887b15e74753dec4f5b7e8079
JORDAN <jordan@home.com>
2020-04-12 18:20:27 +0700

    sadasd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React ,  { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
-
+import Button from '@material-ui/core/Button';
+import KKKK from "./TestButton"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import About from './screen/About'
 const get_user = gql`
 query {
   users {
@@ -16,24 +24,59 @@ query {
 
 function App() {
   const { loading, error, data } = useQuery(get_user)
+
+  const [test, setTest] = useState(true);
+  const [count, setCount] = useState(0);
+
+  // check state if changed
+  // 1   2   3      => 1 2 3
+  useEffect(() => {
+    console.log("chay ham useEffect")
+    //goi API => 
+  });
+
+  useEffect(()=> {
+    console.log("use Effect ==>> ")
+      //get list User
+  }, [])
+
   console.log(data)
   return (
+    <Router>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {}
         </p>
         <a
           className="App-link"
           href="https://reactjs.org"
-          target="_blank"
+          target="_self"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <KKKK test={"blue"}/>
+        <Button onClick={() =>{
+            setTest(!test)
+        }} variant="contained" color={test? "primary" : "secondary"}>
+  {test ? "Primary" : "Secondary"}
+</Button>
+
+<Button onClick={() =>{
+            setCount(count + 1)
+        }} variant="contained" >OK</Button>
+        <li><Link to="/about">About</Link></li>
+      <div onClick={()=>console.log("DIV----")}> Count la {count}</div>
       </header>
     </div>
+    <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch></Router>
   );
 }
 
